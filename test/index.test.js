8edb6f2b62afede4b268bd98b7a59d91d79e3d21
ngryman <ngryman@gmail.com>
2020-03-11 13:59:34 +0100

    chore: use eslint for linting

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,14 @@
-'use strict'
-
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
 const rmdir = require('rimraf')
-const utils = require('../lib/utils')
 
-function clearCRLF (raw) {
+function clearCRLF(raw) {
   return raw.replace(/\r/g, '').trim()
 }
 
-function handleError (err, stats, done) {
+function handleError(err, stats, done) {
   if (err) {
     console.error(err.stack || err)
     if (err.details) {
@@ -21,9 +18,11 @@ function handleError (err, stats, done) {
     return false
   }
 
-  console.log(stats.toString({
-    colors: true    // Shows colors in the console
-  }))
+  console.log(
+    stats.toString({
+      colors: true // Shows colors in the console
+    })
+  )
 
   const info = stats.toJson()
   if (stats.hasErrors()) {
@@ -46,8 +45,14 @@ function runSimpleTest(done, fixtureName) {
     try {
       assert.equal(stats.errors, undefined)
 
-      let css = fs.readFileSync(path.join(__dirname, 'runtime/' + fixtureName + '/index.css'), 'utf8')
-      let expect = fs.readFileSync(path.join(__dirname, 'fixtures/' + fixtureName + '/expect.css'), 'utf8')
+      let css = fs.readFileSync(
+        path.join(__dirname, 'runtime/' + fixtureName + '/index.css'),
+        'utf8'
+      )
+      let expect = fs.readFileSync(
+        path.join(__dirname, 'fixtures/' + fixtureName + '/expect.css'),
+        'utf8'
+      )
 
       assert.equal(clearCRLF(css), clearCRLF(expect))
 
@@ -58,7 +63,7 @@ function runSimpleTest(done, fixtureName) {
   })
 }
 
-describe('test sass-loader', function () {
+describe('test sass-loader', function() {
   this.timeout(10000)
 
   const runtimeDir = path.join(__dirname, 'runtime')
@@ -67,7 +72,7 @@ describe('test sass-loader', function () {
     rmdir(runtimeDir, done)
   })
 
-  it('should load normal sass file', function (done) {
+  it('should load normal sass file', function(done) {
     const config = require('./fixtures/normal/webpack.config.js')
     const compiler = webpack(config)
 
@@ -95,7 +100,7 @@ describe('test sass-loader', function () {
     })
   })
 
-  it('should load sass file with data option', function (done) {
+  it('should load sass file with data option', function(done) {
     const config = require('./fixtures/withData/webpack.config.js')
     const compiler = webpack(config)
 
@@ -119,15 +124,15 @@ describe('test sass-loader', function () {
     })
   })
 
-  it('should compile without options', function (done) {
+  it('should compile without options', function(done) {
     runSimpleTest(done, 'simple')
   })
 
-  it('should auto remove BOM header', function (done) {
+  it('should auto remove BOM header', function(done) {
     runSimpleTest(done, 'bom-issue')
   })
 
-  it('should resolve files with double extensions', function (done) {
+  it('should resolve files with double extensions', function(done) {
     runSimpleTest(done, 'double-extensions')
   })
 
@@ -139,19 +144,19 @@ describe('test sass-loader', function () {
     runSimpleTest(done, 'comment-import')
   })
 
-  it('should be able to resolve @import paths (#39)', function (done) {
+  it('should be able to resolve @import paths (#39)', function(done) {
     runSimpleTest(done, 'data-import-issue')
   })
 
-  it('should pass options to sass.render (#53)', function (done) {
+  it('should pass options to sass.render (#53)', function(done) {
     runSimpleTest(done, 'pass-output-style')
   })
 
-  it('should be able to resolve @import "bulma" (#40)', function (done) {
+  it('should be able to resolve @import "bulma" (#40)', function(done) {
     runSimpleTest(done, 'bulma-issue')
   })
 
-  it('should load normal sass file without url resolving', function (done) {
+  it('should load normal sass file without url resolving', function(done) {
     const config = require('./fixtures/normal-no-url-resolve/webpack.config.js')
     const compiler = webpack(config)
 
@@ -163,13 +168,25 @@ describe('test sass-loader', function () {
       try {
         assert.equal(stats.errors, undefined)
 
-        let css = fs.readFileSync(path.join(__dirname, 'runtime/normal-no-url-resolve/index.css'), 'utf8')
-        let expect = fs.readFileSync(path.join(__dirname, 'fixtures/normal-no-url-resolve/expect.css'), 'utf8')
+        let css = fs.readFileSync(
+          path.join(__dirname, 'runtime/normal-no-url-resolve/index.css'),
+          'utf8'
+        )
+        let expect = fs.readFileSync(
+          path.join(__dirname, 'fixtures/normal-no-url-resolve/expect.css'),
+          'utf8'
+        )
 
         assert.equal(clearCRLF(css), clearCRLF(expect))
 
-        let css2 = fs.readFileSync(path.join(__dirname, 'runtime/normal-no-url-resolve/index2.css'), 'utf8')
-        let expect2 = fs.readFileSync(path.join(__dirname, 'fixtures/normal-no-url-resolve/expect2.css'), 'utf8')
+        let css2 = fs.readFileSync(
+          path.join(__dirname, 'runtime/normal-no-url-resolve/index2.css'),
+          'utf8'
+        )
+        let expect2 = fs.readFileSync(
+          path.join(__dirname, 'fixtures/normal-no-url-resolve/expect2.css'),
+          'utf8'
+        )
 
         assert.equal(clearCRLF(css2), clearCRLF(expect2))
         done()
@@ -179,7 +196,7 @@ describe('test sass-loader', function () {
     })
   })
 
-  it('should accept a different sass implementation', function (done) {
+  it('should accept a different sass implementation', function(done) {
     runSimpleTest(done, 'withSass')
   })
 })
